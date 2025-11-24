892b2ad448b99597a20acd4b01c7a5f7bc19bb31
RufusSamraj <rufussamraj@gmail.com>
2025-11-24 12:01:49 +0530

    changed folder structure

---
+++ apps/ui/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold text-red-500">Hello</div>
+    </>
+  )
+}
+
+export default App
