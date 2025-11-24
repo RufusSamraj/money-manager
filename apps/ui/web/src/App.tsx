0328572f5038f8507774a8b634f2777c6c0d0302
H Samuel Raj <samuelrajholyns@gmail.com>
2025-11-24 14:57:06 +0530

    fixed tailwindcss dependency error

--- apps/ui/web/src/App.tsx
+++ apps/ui/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 
 import TopNavBar from "./components/navbar";
 import StatsPage from "./components/stats/StatsPage";
