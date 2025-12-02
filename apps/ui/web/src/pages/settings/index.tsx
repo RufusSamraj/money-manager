8958a7111d575926cf612c8101ae9d0a1289304f
H Samuel Raj <samuelrajholyns@gmail.com>
2025-12-02 16:37:04 +0530

    minor bug fix: changed placeholder of settings page.

--- apps/ui/web/src/pages/settings/index.tsx
+++ apps/ui/web/src/pages/settings/index.tsx
@@ -4,7 +4,7 @@ export function SettingsPage() {
     return (
         <div className="h-full flex flex-col items-center justify-center text-gray-400">
             <Settings size={48} className="mb-4 opacity-20" />
-            <p>Settings Panel Placeholder</p>
+            <p>Settings Comming soon</p>
         </div>
     )
 }
